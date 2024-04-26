605b868ee508eeb0fffa726e62c55f6714ac5bbf
VasylMolnar <vasylmolnar281@gmail.com>
2024-04-26 15:20:18 +0300

    Update accessRoute.ts

--- routes/accessRoute.ts
+++ routes/accessRoute.ts
@@ -1,8 +1,11 @@
 const router = require('express').Router()
 import {
     handlerAccessReq,
+    handlerAccessReqFollow,
     handlerAccessRes,
+    handlerAccessResFollow,
     handlerDeleteMessage,
+    handlerDeleteMessageFollow,
 } from '../controllers/accessController'
 
 //Send  message to Admin
@@ -13,5 +16,14 @@ router.post('/access-res', handlerAccessRes)
 
 //Delete message
 router.delete('/access-delete', handlerDeleteMessage)
+
+//Send  message to User to follow
+router.post('/follow-access-req', handlerAccessReqFollow)
+
+//Get message from User
+router.post('/follow-access-res', handlerAccessResFollow)
+
+//Delete User message
+router.delete('/follow-access-delete', handlerDeleteMessageFollow)
 
 module.exports = router
